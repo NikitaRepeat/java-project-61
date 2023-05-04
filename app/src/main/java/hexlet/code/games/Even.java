254b6680d5a456095748567238fc281c0453b0df
Piskun Nikita <n.piskun2014@yandex.ru>
2023-05-05 00:31:40 +0300

    fix Even.java add enter to Question

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -4,7 +4,7 @@ public class Even {
     private static final int MULTIPLE = 100;
     public static String evenGame() {
         int randomNumber = (int) (Math.random() * MULTIPLE);
-        System.out.println("Question:" + randomNumber);
+        System.out.println("Question: " + randomNumber);
         System.out.print("Your answer: ");
         if (randomNumber % 2 == 0) {
             return "yes";
