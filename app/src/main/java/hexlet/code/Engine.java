0c36c094e5b95eb5041fc401f760bb247ed501ab
Piskun Nikita <n.piskun2014@yandex.ru>
2023-05-10 14:12:00 +0300

    add public static final in engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -8,34 +8,34 @@ import hexlet.code.games.Prime;
 
 import java.util.Scanner;
 public class Engine {
-    private static int choice1 = 1;
-    private static int choice2 = 2;
-    private static int choice3 = 3;
-    private static int choice4 = 4;
-    private static int choice5 = 5;
-    private static int choice6 = 6;
+    private static final int CHOICE1 = 1;
+    private static final int CHOICE2 = 2;
+    private static final int CHOICE3 = 3;
+    private static final int CHOICE4 = 4;
+    private static final int CHOICE5 = 5;
+    private static final int CHOICE6 = 6;
     private static final int NUMBER_ATTEMPTS = 2;
 
     public static void choiceGames(int choiceUser) {
-        if (choiceUser == choice1) {
+        if (choiceUser == CHOICE1) {
             Cli.welcomeUser();
-        } else if (choiceUser == choice2) {
+        } else if (choiceUser == CHOICE2) {
             Cli.welcomeUser();
             System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
             game(choiceUser);
-        } else if (choiceUser == choice3) {
+        } else if (choiceUser == CHOICE3) {
             Cli.welcomeUser();
             System.out.println("What is the result of the expression?");
             game(choiceUser);
-        } else if (choiceUser == choice4) {
+        } else if (choiceUser == CHOICE4) {
             Cli.welcomeUser();
             System.out.println("Find the greatest common divisor of given numbers.");
             game(choiceUser);
-        } else if (choiceUser == choice5) {
+        } else if (choiceUser == CHOICE5) {
             Cli.welcomeUser();
             System.out.println("What number is missing in the progression?");
             game(choiceUser);
-        } else if (choiceUser == choice6) {
+        } else if (choiceUser == CHOICE6) {
             Cli.welcomeUser();
             System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
             game(choiceUser);
@@ -52,15 +52,15 @@ public class Engine {
         boolean runGame = true;
         boolean winTitle = false;
         while (runGame) {
-            if (choiceUser == choice2) {
+            if (choiceUser == CHOICE2) {
                 answerRight = Even.evenGame();
-            } else if (choiceUser == choice3) {
+            } else if (choiceUser == CHOICE3) {
                 answerRight = Calc.calculator();
-            } else if (choiceUser == choice4) {
+            } else if (choiceUser == CHOICE4) {
                 answerRight = GCD.gcdGame();
-            } else if (choiceUser == choice5) {
+            } else if (choiceUser == CHOICE5) {
                 answerRight = Progression.proGame();
-            } else if (choiceUser == choice6) {
+            } else if (choiceUser == CHOICE6) {
                 answerRight = Prime.primeGame();
             } else {
                 System.out.println("Goodbye!");
