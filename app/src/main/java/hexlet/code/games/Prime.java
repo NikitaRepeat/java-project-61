ae8ed33d384e5fa1e140080d294db037b42f54ec
Piskun Nikita <n.piskun2014@yandex.ru>
2023-05-10 13:51:58 +0300

    fix Prime game

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -2,7 +2,7 @@ package hexlet.code.games;
 
 public class Prime {
     public static String primeGame() {
-        int randPrimeNumber = (int) (1 + Math.random() * 10);
+        int randPrimeNumber = (int) (2 + Math.random() * 100);
         System.out.println("Question: " + randPrimeNumber);
         System.out.println("Your answer: ");
         for (int i = 2; i < randPrimeNumber; i++) {
